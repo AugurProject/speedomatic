ec5a522149c7ec88a01a7b7be38bf3926d3fecf2
Jack Peterson <jack@tinybike.net>
2016-07-23 01:07:07 -0700

    Incremented version

--- dist/augur-abi.js
+++ dist/augur-abi.js
@@ -23,7 +23,7 @@ module.exports = {
 
     debug: false,
 
-    version: "0.5.4",
+    version: "0.5.6",
 
     constants: {
         ONE: new BigNumber(10).toPower(new BigNumber(18)),
--- index.js
+++ index.js
@@ -15,7 +15,7 @@ module.exports = {
 
     debug: false,
 
-    version: "0.5.4",
+    version: "0.5.6",
 
     constants: {
         ONE: new BigNumber(10).toPower(new BigNumber(18)),
