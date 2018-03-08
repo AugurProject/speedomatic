9cdef7bd4c67450769a2f3cd9b5dd8055442b2a9
Paul Gebheim <pgebheim@users.noreply.github.com>
2018-03-08 11:41:20 -0800

    Update abi-decode-rpc-response.js

--- src/abi-decode-rpc-response.js
+++ src/abi-decode-rpc-response.js
@@ -1,7 +1,6 @@
 "use strict";
 
 var abiDecodeData = require("./abi-decode-data");
-var formatInt256 = require("./format-int256");
 
 function abiDecodeRpcResponse(responseType, abiEncodedRpcResponse) {
   var decodedRpcResponse = abiDecodeData([{type: responseType}], abiEncodedRpcResponse)[0];
